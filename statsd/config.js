72f75d84326212e082617e13e4a8897bd9ae3203
Matthew Williams <mrwillihog@users.noreply.github.com>
2014-06-04 11:25:57 +0100

    Remove StatsD legacy namespaces

--- statsd/config.js
+++ statsd/config.js
@@ -1,8 +1,10 @@
 {
   graphitePort: 2003,
   graphiteHost: "127.0.0.1",
+  graphite: {
+    legacyNamespace: false
+  },
   port: 8125,
   mgmt_port: 8126,
-  backends: ['./backends/graphite'],
-  debug: true
+  backends: ['./backends/graphite']
 }
